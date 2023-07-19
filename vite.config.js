f89dd7fd22d9cda08c46958d2b0e92632d7b7ad3
Mo Khalil <mokhalil3@hotmail.com>
2023-07-19 14:13:05 -0500

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
    plugins: [vue(),
 
             Pages({
-               dirs: 'src/views',
+               dirs: './src/views',
              })  ],
    resolve: {
      alias: {
