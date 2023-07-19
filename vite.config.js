efcac53adf923f1a6a2dfd91aa686d5f96e2eec2
Mo Khalil <mokhalil3@hotmail.com>
2023-07-19 13:31:16 -0500

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import Pages from 'vite-plugin-pages'
 export default defineConfig({
    //when deploying 
    build: {
-      outDir: '/School_MS-frontend/',
+      outDir: '/docs',
       emptyOutDir: false
    },
    plugins: [vue(),
